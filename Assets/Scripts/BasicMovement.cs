771466caec1489bcb5ce252b5a51027a488040b0
Juno AM <sabotaione@gmail.com>
2023-09-14 19:02:03 -0400

    transferred files over from project

---
+++ Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    /* 
+        this is a comment
+    */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)){
+            Debug.Log("W key pressed");
+            transform.position += Vector3.forward;
+        }
+    }
+}
