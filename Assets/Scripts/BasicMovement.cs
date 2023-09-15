7ac91052af82e2aa00c1f903f9aec7c3c7dbea89
Juno AM <sabotaione@gmail.com>
2023-09-14 20:51:55 -0400

    added week 3 contents

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
-    /* 
-        this is a comment
-    */
+    public GameObject hazard;
+    public GameObject[] hazards;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -20,6 +18,36 @@ public class BasicMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.W)){
             Debug.Log("W key pressed");
             transform.position += Vector3.forward;
+        }
+        if (Input.GetKeyDown(KeyCode.A)){
+            transform.position += Vector3.left;
+        }
+        if (Input.GetKeyDown(KeyCode.D)){
+            transform.position += Vector3.right;
+        }
+        if (Input.GetKeyDown(KeyCode.S)){
+            transform.position += Vector3.back;
+        }
+        if (Input.GetKeyDown(KeyCode.Q)){
+            transform.position += Vector3.down;
+        }
+        if (Input.GetKeyDown(KeyCode.E)){
+            transform.position += Vector3.up;
+        }
+/*
+        if (transform.position == hazard.transform.position){
+           Debug.Log("you are hazards ): lul");
+           Destroy(gameObject);
+        }
+*/
+        //for loop ... the first part is a declaration and assignment for a variable to iterate through our loop
+        //the second part is a condition .. after each run, it will check if this is true 
+        //the third part will update i after each time it runs
+        for (int i = 0; i < hazards.Length; i++){
+           if (transform.position == hazards[i].transform.position){
+           Debug.Log("you are hazards ): lul");
+           Destroy(gameObject);
+           }
         }
     }
 }
