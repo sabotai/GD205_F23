8830007ded6bd37bef27fe911b59f1f4d6b40928
Juno AM <sabotaione@gmail.com>
2023-11-09 20:18:18 -0500

    added week 11 ui stuff

---
+++ Assets/Scripts/UIFunctions.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIFunctions : MonoBehaviour
+{
+    int myIntName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Dice roll = " + DiceRoll());
+        Debug.Log("Rigged roll = " + RiggedDiceRoll()); 
+    }
+
+    int DiceRoll(){
+        int dRoll = Random.Range(0, 6);
+
+        return dRoll;
+    }
+
+    int RiggedDiceRoll(){
+        return 6;
+    }
+
+    public void ChangeScene(){
+        //add code to change scene
+        SceneManager.LoadScene("Week3");
+    }
+}
